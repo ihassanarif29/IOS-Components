c8324b6ee9635c37122786b0622274f693e4991d
ihassanarif29 <ihassanarif29@gmail.com>
2026-04-28 00:49:50 +0500

    Initial Commit

---
+++ IOS Components/IOS_ComponentsApp.swift
@@ -0,0 +1,17 @@
+//
+//  IOS_ComponentsApp.swift
+//  IOS Components
+//
+//  Created by Hassan on 28/4/26.
+//
+
+import SwiftUI
+
+@main
+struct IOS_ComponentsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
