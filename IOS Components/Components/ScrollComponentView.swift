b1f19611769228dce0a0d3ea9acf4e889e044fa6
Hassan Arif <ihassanarif29@gmail.com>
2026-04-30 03:45:15 +0500

    Add Scroll Component

---
+++ IOS Components/Components/ScrollComponentView.swift
@@ -0,0 +1,18 @@
+//
+//  ScrollComponentView.swift
+//  IOS Components
+//
+//  Created by Hassan on 30/4/26.
+//
+
+import SwiftUI
+
+struct ScrollComponentView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ScrollComponentView()
+}
